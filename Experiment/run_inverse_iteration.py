aebff97ce0889e54c3dca7f4e66c535eca53c431
Shuo from SCCS work station <sunshuo987@gmail.com>
2026-02-23 16:16:52 +0100

    change inverse iteration orthogonalization

--- Experiment/run_inverse_iteration.py
+++ Experiment/run_inverse_iteration.py
@@ -9,7 +9,7 @@ from typing import List
 from pytreenet.operators import TensorProduct, Hamiltonian
 from pytreenet.ttno import TreeTensorNetworkOperator
 from pytreenet.util import SVDParameters
-from pytreenet.util.misc_functions import orthogonalise_gep, linear_combination
+from pytreenet.util.misc_functions import orthogonalise_gep, linear_combination, orthogonalise_to
 from pytreenet.ttns import TreeTensorNetworkState
 from pytreenet.dmrg.als import AlternatingLeastSquares
 
@@ -18,7 +18,7 @@ from utils import get_energy_clusters, get_ttno
 
 np.random.seed=42
 
-def run_block_inverse_iteration(ham_pad: Hamiltonian, ttnss: List[TreeTensorNetworkState], save_to_path: str,num_sweeps = 5, svd_params: SVDParameters = SVDParameters(max_bond_dim=10, rel_tol=1e-5, total_tol=1e-5)):
+def run_block_inverse_iteration(ham_pad: Hamiltonian, ttnss: List[TreeTensorNetworkState], save_to_path: str,num_sweeps = 5, svd_params: SVDParameters = SVDParameters(max_bond_dim=10, rel_tol=1e-5, total_tol=1e-5), file_path: List[str] = []):
       
     es_ii = []
     ttno = TreeTensorNetworkOperator.from_hamiltonian(ham_pad, ttnss[0], dtype=np.float64)
@@ -46,11 +46,12 @@ def run_block_inverse_iteration(ham_pad: Hamiltonian, ttnss: List[TreeTensorNetw
         es_ii.append(es_i)
         if len(ttnss) > 1:
             ttnss = orthogonalise_gep(ttno, ttnss, min(svd_params.max_bond_dim, max(ttnss[0].bond_dims().values())), num_sweeps = 1)
-
-            for k in range(len(ttnss)):
-                print(f"Sweep {i}, State {k}, Energy {ttnss[k].operator_expectation_value(ttno)}")
         else:
             ttnss[0] = linear_combination(ttnss, [1.0], min(svd_params.max_bond_dim, max(ttnss[0].bond_dims().values())), num_sweeps=1)
+        if len(file_path) > 0:
+            print("Orthogonalising to", file_path)
+            for ttns in ttnss:
+                ttns = orthogonalise_to(ttns, file_path, svd_params.max_bond_dim, num_sweeps)
     return np.array(es_ii).T.tolist(), ttnss
 
 def main(results_dir, num_sweeps = 10, max_bond_dim = 12):
@@ -62,7 +63,7 @@ def main(results_dir, num_sweeps = 10, max_bond_dim = 12):
     print("Clusters", clusters)
     state_dir = f"{results_dir}/states"
     
-    reference_energy = pd.read_csv("./Experiment/ch3cn_ref.csv")["Energy_Ref"].values
+    reference_energy = pd.read_csv("./Experiment/ch3cn_ref.csv")["Energy_Ref"].to_numpy(copy=True)
     reference_energy[1:] = reference_energy[1:] + reference_energy[0]
     
     N = [9,7,9,9,9,9,7,7,9,9,27,27]
@@ -71,15 +72,17 @@ def main(results_dir, num_sweeps = 10, max_bond_dim = 12):
 
     cl_state_cnt = 0
     energies = []
+    file_path = []#[f"{state_dir}/inverse_iteration_state_{i}_wresi_20" for i in range(0,cl_state_cnt)]
     for ix, cl in enumerate(clusters[0:]):
-        # cl+=clusters[0]
+        # cl=cl+clusters[20]+clusters[21]
         time_start = time.time()
         print(f"Cluster {ix}", cl)
         ttnss = [TreeTensorNetworkState.load(state_dir + f"/lobpcg_state_{i}") for i in cl]
         
-        es_ii, ttnss = run_block_inverse_iteration(ham_pad, ttnss, state_dir, num_sweeps = num_sweeps, svd_params = SVDParameters(max_bond_dim=max_bond_dim, rel_tol=1e-10, total_tol=1e-10))
+        es_ii, ttnss = run_block_inverse_iteration(ham_pad, ttnss, state_dir, num_sweeps = num_sweeps, svd_params = SVDParameters(max_bond_dim=max_bond_dim, rel_tol=1e-10, total_tol=1e-10), file_path = file_path)
         energies.append(es_ii)
         time_end = time.time()
+        file_path = [f"{state_dir}/inverse_iteration_state_{i}_wresi_20" for i in range(cl_state_cnt, cl_state_cnt + len(cl))]
         print(f"Time taken: {time_end - time_start} seconds")
         for k in range(len(es_ii)):
             plt.plot(abs(np.array(es_ii[k])*1000-reference_energy[cl_state_cnt + k]), label="inverse iteration")
@@ -96,17 +99,17 @@ def main(results_dir, num_sweeps = 10, max_bond_dim = 12):
             ttnss[k].save(f"{state_dir}/inverse_iteration_state_{cl_state_cnt + k}_wresi_20")
             print("bond dim", k, ttnss[k].bond_dims().values())    
         cl_state_cnt += len(cl)
-        np.savetxt(f"{results_dir}/inverse_iteration_energies_cache_20.csv", np.concatenate(energies,axis = 0), delimiter=",")
+        np.savetxt(f"{results_dir}/inverse_iteration_energies_cache_20_cl19.csv", np.concatenate(energies,axis = 0), delimiter=",")
 
     energies = np.concatenate(energies,axis = 0)
-    np.savetxt(f"{results_dir}/inverse_iteration_energies_20.csv", np.array(energies), delimiter=",")
+    np.savetxt(f"{results_dir}/inverse_iteration_energies_20_cl19.csv", np.array(energies), delimiter=",")
     profiler.disable()
-    profiler.dump_stats(f"{results_dir}/profile_output_ii_20.prof")
+    profiler.dump_stats(f"{results_dir}/profile_output_ii_20_cl19.prof")
 
 if __name__ == "__main__":
-    main(results_dir = "./Results_final/ch3cn/mps/max_bond_dim_12", 
-         num_sweeps = 5, max_bond_dim = 20)
-    main(results_dir = "./Results_final/ch3cn/threetree/max_bond_dim_12", 
-         num_sweeps = 5, max_bond_dim = 20)
-    main(results_dir = "./Results_final/ch3cn/leafonly/max_bond_dim_12", 
+    # main(results_dir = "./Results_test/ch3cn/mps/max_bond_dim_12", 
+    #      num_sweeps = 5, max_bond_dim = 20)
+    # main(results_dir = "./Results_test/ch3cn/threetree/max_bond_dim_12", 
+    #      num_sweeps = 5, max_bond_dim = 20)
+    main(results_dir = "./Results_test/ch3cn/leafonly/max_bond_dim_12", 
          num_sweeps = 5, max_bond_dim = 20)
